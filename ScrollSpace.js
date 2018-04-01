6310909a9c938875992fba7c3c69ea1a3f272f48
Erik Zetterström <erik@ictenablers.com>
2018-04-01 17:08:28 +0200

    Update ScrollSpace.js
    Compact

--- ScrollSpace.js
+++ ScrollSpace.js
@@ -1,9 +1,1 @@
-;(function(){
-  
-  var init = function() {
-    document.getElementsByTagName("body")[0].style.marginLeft="10%";
-  }
-
-init();
-	
-})(window);
+(function(){document.getElementsByTagName("body")[0].style.marginLeft="10%";})();
